bfc4665a74fc8893e6f798f7d516601480d52639
vurtn <vurtn@protonmail.com>
2021-10-16 15:43:35 +0200

    styling and refacto

--- assets/js/dice.js
+++ assets/js/dice.js
@@ -1,41 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".dice").forEach(element => element.onclick = () => {
-    roll(element.dataset.faces)
-  })
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelectorAll(".dice").forEach(element => element.onclick = () => {
+		roll(element.dataset.faces)
+	})
 });
 
 const rand = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+	return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 const roll = (faces = 6) => {
+	const int_faces = parseInt(faces, 10);
+	if (isNaN(int_faces)) {
+		return;
+	}
 
-  if (faces === "fate") {
-    roll_fate()
-  }
-
-  const int_faces = parseInt(faces, 10);
-  if (isNaN(int_faces)) { return; }
-
-  roll_classic(int_faces)
+	roll_classic(int_faces)
 }
 
 const roll_classic = (faces) => {
-  value = rand(1, faces);
-  document.querySelector("#fate-dice").innerHTML = ""
-  document.querySelector("#value").innerHTML = value
-}
-
-const roll_fate = () => {
-  values = new Array(4).fill(0).map((_) => rand(-1, 1))
-  value = values.reduce((acc, numb) => acc + numb, 0)
-  document.querySelector("#value").innerHTML = value
-  document.querySelector("#fate-dice").innerHTML = values.map((val => {
-    if (val < 0) {
-      return '<i class="fa fa-minus-square-o fate-dice" aria-hidden="true"></i>'
-    } else if (val > 0) {
-      return '<i class="fa fa-plus-square-o fate-dice" aria-hidden="true"></i>'
-    }
-    return '<i class="fa fa-square-o fate-dice" aria-hidden="true"></i>'
-  })).reduce((acc, el) => acc + el)
+	value = rand(1, faces);
+	document.querySelector("#value").innerHTML = value
 }
