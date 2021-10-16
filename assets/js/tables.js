1f40a3293ee6b51d30b9205dca187ad17cb0131c
vurtn <vurtn@protonmail.com>
2021-10-16 23:37:40 +0200

    roll in a table and some design on index

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -3,7 +3,6 @@ const table_pick = (event) => {
 	const name = row.children[0].firstChild.textContent
 	const trs = [...row.querySelectorAll(".table-content table tr")]
 	trs.shift() //remove the header
-	const items = trs.map(tr => tr.lastElementChild.textContent)
-	console.log(name)
-	console.log(items)
+	const items = trs.map(tr => tr.lastElementChild.textContent);
+	row.cells[2].textContent = items[Math.floor(Math.random() * (items.length - 1 + 1))]
 }
